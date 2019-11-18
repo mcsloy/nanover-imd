a9d7d188406e04e0c58c51c189cf0f845761ae4d
Alex Binnie <alexjbinnie@gmail.com>
2019-11-18 18:34:11 +0000

    Implemented ESSD

---
+++ Assets/NarupaIMD/ServiceDiscovery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Essd;
+using Narupa.Core.Async;
+using NarupaXR;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace NarupaIMD
+{
+    /// <summary>
+    /// Autoconnect to the first server we find.
+    /// </summary>
+    public class ServiceDiscovery : MonoBehaviour
+    {
+        [SerializeField]
+        private NarupaXRPrototype prototype;
+
+        private void Start()
+        {
+            var client = new Client();
+            client.StartSearch().AwaitInBackground();
+            client.ServiceFound += ClientOnServiceFound;
+        }
+        private readonly HashSet<ServiceHub> services = new HashSet<ServiceHub>();
+
+        public event  Action<ServiceHub> ServiceDiscovered;
+        
+        private void ClientOnServiceFound(object sender, ServiceHub e)
+        {
+            if (!services.Add(e))
+                return;
+
+            ServiceDiscovered?.Invoke(e);
+        }
+
+        /// <summary>
+        /// Connect to the given service hub.
+        /// </summary>
+        public void Connect(ServiceHub e)
+        {
+            var services = e.Properties["services"] as JObject;
+            prototype.Connect(e.Address, services["trajectory"].ToObject<int>(),
+                              services["imd"].ToObject<int>(),
+                              services["multiplayer"].ToObject<int>());
+        }
+    }
+}
---
+++ Assets/NarupaIMD/UI/ServiceList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Essd;
+using Narupa.Frontend.UI;
+using NarupaIMD;
+using UnityEngine;
+
+public class ServiceList : MonoBehaviour
+{
+    [SerializeField]
+    private NarupaButton prefab;
+
+    [SerializeField]
+    private ServiceDiscovery services;
+
+    private void Awake()
+    {
+        services.ServiceDiscovered += ServicesOnServiceDiscovered;
+    }
+
+    private void ServicesOnServiceDiscovered(ServiceHub obj)
+    {
+        var button = Instantiate(prefab, transform);
+        button.gameObject.SetActive(true);
+        button.Text = obj.Name;
+        button.OnClick += () => services.Connect(obj);
+    }
+}
