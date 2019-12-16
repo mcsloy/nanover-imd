f5df492b088d05fe8461584e30aaf20326811436
Mark Wonnacott <ragzouken@gmail.com>
2019-12-16 16:42:42 +0000

    fix test server

--- Assets/Plugins/Narupa/Grpc/Tests/Commands/CommandServer.cs
+++ Assets/Plugins/Narupa/Grpc/Tests/Commands/CommandServer.cs
@@ -9,18 +9,16 @@ namespace Narupa.Grpc.Tests.Commands
     internal class CommandServer : Command.CommandBase
     {
         public event Action<string, Struct> RecievedCommand;
-        
+
         public override async Task<CommandReply> RunCommand(CommandMessage request, ServerCallContext context)
         {
             RecievedCommand?.Invoke(request.Name, request.Arguments);
             return new CommandReply();
         }
 
-        public override async Task GetCommands(GetCommandsRequest request,
-                                         IServerStreamWriter<CommandMessage> responseStream,
-                                         ServerCallContext context)
+        public override async Task<GetCommandsReply> GetCommands(GetCommandsRequest request, ServerCallContext context)
         {
-            return;
+            return new GetCommandsReply();
         }
     }
 }
