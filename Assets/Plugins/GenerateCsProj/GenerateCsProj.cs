bd531596418e04d9571af86d69a6c65cd686c469
Alex Binnie <alexjbinnie@gmail.com>
2020-05-01 00:48:41 +0100

    Try simple addition of GenerateAll

--- Assets/Plugins/GenerateCsProj/GenerateCsProj.cs
+++ Assets/Plugins/GenerateCsProj/GenerateCsProj.cs
@@ -5,6 +5,7 @@ public static class GenerateCsProj
     public static void Generate()
     {
         var gen = new ProjectGeneration();
+        gen.GenerateAll(true);
         gen.Sync();
     }
 }
