91f2a528eb871808036ccdebaff53c4eecbebe0e
Alex Binnie <alexjbinnie@gmail.com>
2020-06-26 15:23:36 +0100

    Remove debugger from UI branch

--- Assets/NarupaIMD/NarupaIMDPrototype.cs
+++ Assets/NarupaIMD/NarupaIMDPrototype.cs
@@ -23,9 +23,6 @@ namespace NarupaIMD
         private NarupaImdSimulation simulation;
         
         private InteractableScene interactableScene;
-
-        [SerializeField]
-        private NarupaXRDebugger debugger;
 #pragma warning restore 0649
 
         public NarupaImdSimulation Simulation => simulation;
@@ -43,8 +40,6 @@ namespace NarupaIMD
             isColocationActive = Colocation.IsEnabled();
         }
 
-        public NarupaXRDebugger Debugger => debugger;
-        
         /// <summary>
         /// Connect to remote Narupa services.
         /// </summary>
